e7a6682b2215485544b8f4a5203fd806cfba866e
Aditya Pawar <apawar@berkeley.edu>
2024-06-23 11:17:49 -0700

    Fix

--- waves/src/content.tsx
+++ waves/src/content.tsx
@@ -4,6 +4,8 @@ import App from "~features/app"
 import { Storage } from "@plasmohq/storage"
 import { getPort } from "@plasmohq/messaging/port"
 import type { PortName } from "@plasmohq/messaging"
+import { useEffect } from "react"
+import { log } from "console"
 
 export const config: PlasmoCSConfig = {}
 
@@ -17,45 +19,61 @@ const storage = new Storage();
 const cloudPort = getPort("cloud" as PortName);
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+let recognition = new SpeechRecognition();
 
-recognition.continuous = true;
+recognition.continuous = false;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+document.addEventListener("visibilitychange", async (event) => {
+  console.log("SROTNOERSINOITRSN");
+  event.preventDefault()
+  if (document.visibilityState == "visible") {
+    const muted: boolean = await storage.get("muted");
+    if (!muted) {
+      recognition = new SpeechRecognition();
+      recognition.start();
+    }
+  } else {
+    recognition.stop()
+  }
+});
+
 recognition.onresult = (event: SpeechRecognitionEvent) => {
-    const transcript: string = event.results[0][0].transcript;
-    cloudPort.postMessage({
-      body: {
-        transcript: transcript
-      }
-    })
-    console.log('transcript: ', transcript);
+  const transcript: string = event.results[0][0].transcript;
+  cloudPort.postMessage({
+    body: {
+      transcript: transcript
+    }
+  })
+  console.log('transcript: ', transcript);
 };
 
 recognition.onerror = (event: SpeechRecognitionErrorEvent) => {
-    console.error('Error occurred in recognition:', event.error);
+  console.error('Error occurred in recognition:', event.error);
 };
 
 recognition.onend = async () => {
-    const muted: boolean = await storage.get("muted");
-    if ( muted === true) {
-      return;
+  const muted: boolean = await storage.get("muted");
+  if (muted === true) {
+    return;
+  }
+  setTimeout(() => {
+    if (!document.hidden) {
+      recognition.start();
     }
-    // recognition.start();
+  }, 300)
 };
-
-recognition.start();
 
 storage.watch({
   "muted": (c) => {
-      if (c.newValue == true) {
-        recognition.stop();
-      } else {
-        recognition.start();
-      }
-      console.log(c.newValue);
+    if (c.newValue == true) {
+      recognition.stop();
+    } else {
+      recognition.start();
+    }
+    console.log(c.newValue);
   }
 })
 
